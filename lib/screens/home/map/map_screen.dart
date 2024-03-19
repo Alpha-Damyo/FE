10d2410b0c8130791def8f103276fa038677e259
kevinmj12 <kevinmj12@gmail.com>
2024-03-19 19:16:35 +0900

    #7 앱 시작 시 내 위치를 중심으로 설정

--- lib/screens/home/map/map_screen.dart
+++ lib/screens/home/map/map_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'dart:developer';
+import 'package:damyo/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:go_router/go_router.dart';
@@ -18,33 +19,7 @@ class _MapScreenState extends State<MapScreen>
   @override
   bool get wantKeepAlive => true;
 
-  // 사용자의 현재 위치를 받아오는 함수
-
-  // void _getCurrentLocation() async {
-  //   Location location = Location();
-  //   bool serviceEnabled;
-  //   PermissionStatus permissionGranted;
-  //   LocationData locationData;
-
-  //   serviceEnabled = await location.serviceEnabled();
-  //   if (!serviceEnabled) {
-  //     serviceEnabled = await location.requestService();
-  //     if (!serviceEnabled) {
-  //       return;
-  //     }
-  //   }
-
-  //   permissionGranted = await location.hasPermission();
-  //   if (permissionGranted == PermissionStatus.denied) {
-  //     permissionGranted = await location.requestPermission();
-  //     if (permissionGranted != PermissionStatus.granted) {
-  //       return;
-  //     }
-  //   }
-
-  //   locationData = await location.getLocation();
-  //   print(locationData.longitude);
-  // }
+  // Position? userPosition; // 사용자의 현재 위치
 
   // 지도  핵심 worker
   NaverMapController? mapController;
@@ -72,8 +47,7 @@ class _MapScreenState extends State<MapScreen>
     if (d == null) {
       return 'null';
     } else {
-      var s = d.toString().split('.');
-      return '${s[0]}.${s[1].substring(0, 5)}';
+      return d.toStringAsFixed(5);
     }
   }
 
@@ -94,17 +68,16 @@ class _MapScreenState extends State<MapScreen>
     // NaverMapController 객체의 비동기 작업 완료를 나타내는 Completer 생성
     final Completer<NaverMapController> mapControllerCompleter = Completer();
 
-    // _getCurrentLocation();
     return Scaffold(
       body: Stack(
         children: [
           // 지도 화면
           NaverMap(
-            options: const NaverMapViewOptions(
-              // initialCameraPosition: NCameraPosition(
-              // target: NLatLng(37.56659, 126.97899),
-              //   zoom: 14.0,
-              // ),
+            options: NaverMapViewOptions(
+              initialCameraPosition: NCameraPosition(
+                target: NLatLng(userLatitude, userLongitude),
+                zoom: 14.0,
+              ),
               locationButtonEnable: true, // 위치 버튼 표시 여부 설정
               consumeSymbolTapEvents: false, // 심볼 탭 이벤트 소비 여부 설정
             ),
