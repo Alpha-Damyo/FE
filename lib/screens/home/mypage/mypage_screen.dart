67de3376c27db804ba7d394a9609d272a947f67b
ZomSik <sunberbee@naver.com>
2024-03-28 01:53:09 +0900

    #29 mypageScreen 오류 수정

--- lib/screens/home/mypage/mypage_screen.dart
+++ lib/screens/home/mypage/mypage_screen.dart
@@ -1,32 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class MypageScreen extends StatelessWidget {
-  const MypageScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Text(
-          '마이페이지 들어갈 페이지',
-          style: TextStyle(
-            fontSize: 40,
-          ),
-        ),
-        IconButton(
-            onPressed: () {
-              context.push('/login');
-            },
-            icon: const Icon(Icons.login)),
-      ],
-    );
-  }
-}
-/*import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-
 class MypageScreen extends StatefulWidget {
   const MypageScreen({super.key});
 
@@ -449,4 +423,4 @@ class _MypageScreenState extends State<MypageScreen> {
       ],
     );
   }
-}*/
+}
