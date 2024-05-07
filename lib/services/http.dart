a61a9017fe0ae3321be5ff5b6cc198101a26f2b3
kevinmj12 <kevinmj12@gmail.com>
2024-05-07 23:40:03 +0900

    tmp

--- lib/services/http.dart
+++ lib/services/http.dart
@@ -1,6 +1,7 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'secret.dart';
+import '../secret.dart';
 
 Future<String> GetAddress(String coords) async {
   const String sourcecrs = 'epsg:4326';
@@ -11,8 +12,8 @@ Future<String> GetAddress(String coords) async {
     "https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?coords=$coords&sourcecrs=$sourcecrs&orders=$orders&output=$output",
   );
   var response = await http.get(url, headers: {
-    'X-NCP-APIGW-API-KEY-ID': secretNaverCloudId,
-    'X-NCP-APIGW-API-KEY': secretNaverCloudSecret,
+    'X-NCP-APIGW-API-KEY-ID': dotenv.get('NAVER_CLOUD_ID'),
+    'X-NCP-APIGW-API-KEY': dotenv.get('NAVER_CLOUD_SECRET'),
   });
 
   // 성공
@@ -52,13 +53,3 @@ Future<String> GetAddress(String coords) async {
     return '에러: ${response.statusCode}, 주소를 불러올 수 없습니다.';
   }
 }
-
-// void Inform() async {
-//   var url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
-//   var response = await http.post(
-//     url,
-//     body: {
-
-//     },
-//   );
-// }
