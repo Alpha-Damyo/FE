e96c38dfbf4f5782f671d2d187dbde156ea5d0e2
choichangyeon <changyon99@naver.com>
2024-05-27 15:34:51 +0900

    Revert "Merge commit 'e2af08aacee10f3a9977c427505ff2cdf0ee5e9a' into Feature/#39-StatisticsScreen"
    This reverts commit 608996937e77adb3d0719f74c2b8682365636ed1, reversing
    changes made to a38aef9eaa9aef4d2f17a700a5bb22450c4ee3d5.

--- lib/screens/home/challenge/challengedetail_screen.dart
+++ lib/screens/home/challenge/challengedetail_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:share_plus/share_plus.dart';
 
@@ -10,47 +9,47 @@ class ChallengeDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(390, 667),
-      builder: (context, child) => Scaffold(
-        appBar: AppBar(
-          title: const Text('챌린지'),
-          centerTitle: true,
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back),
-            onPressed: () => Navigator.of(context).pop(),
-          ),
-          scrolledUnderElevation: 0,
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.black,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('챌린지'),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
         ),
         backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Column(
+        foregroundColor: Colors.black,
+      ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 8),
+              child: Column(
                 children: [
                   Container(
-                    width: 390.w,
-                    height: 163.h,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
+                    width: MediaQuery.of(context).size.width - 16,
+                    height: 163,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      image: const DecorationImage(
                         image:
-                            NetworkImage("https://via.placeholder.com/390x163"),
+                            NetworkImage("https://via.placeholder.com/358x163"),
                         fit: BoxFit.fill,
                       ),
                     ),
                   ),
                   Padding(
-                    padding: EdgeInsets.fromLTRB(18.w, 8.h, 18.w, 8.h),
+                    padding: const EdgeInsets.fromLTRB(10, 8, 10, 8),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Text(
                           '$title : 2024.1.1 ~ 2024.6.30',
-                          style: TextStyle(
-                            color: const Color(0xFF262B32),
-                            fontSize: 14.sp,
+                          style: const TextStyle(
+                            color: Color(0xFF262B32),
+                            fontSize: 14,
                             fontFamily: 'Pretendard',
                             fontWeight: FontWeight.w500,
                           ),
@@ -67,35 +66,49 @@ class ChallengeDetailScreen extends StatelessWidget {
                   ),
                 ],
               ),
-              InkWell(
-                onTap: () {
-                  GoRouter.of(context).push('/vote', extra: {'title': title});
-                },
-                splashColor: Colors.transparent,
-                highlightColor: Colors.transparent,
-                child: Container(
-                  height: 37.h,
-                  alignment: Alignment.center,
-                  margin: EdgeInsets.symmetric(horizontal: 16.w),
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFEEF1F4),
-                    borderRadius: BorderRadius.circular(22.r),
-                  ),
-                  child: Text(
-                    '사진 구경하기',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 14.sp,
-                      fontFamily: 'Pretendard',
-                      fontWeight: FontWeight.w600,
-                    ),
+            ),
+            InkWell(
+              onTap: () {
+                // Navigate to the ChallengeVoteScreen()
+                GoRouter.of(context).push('/vote', extra: {'title': title});
+              },
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+              child: Container(
+                height: 37,
+                alignment: Alignment.center,
+                margin: const EdgeInsets.symmetric(
+                    horizontal: 16), // Adjust margin for full width
+                decoration: BoxDecoration(
+                  color: const Color(0xFFEEF1F4),
+                  borderRadius: BorderRadius.circular(22),
+                ),
+                child: const Text(
+                  '사진 구경하기',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                    fontFamily: 'Pretendard',
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
               ),
-              SizedBox(height: 12.h),
-              Container(
-                width: 390.w,
-                height: 624.h,
+            ),
+            const Positioned(
+              left: 360,
+              top: 61,
+              child: SizedBox(
+                width: 12,
+                height: 12,
+                child: Stack(children: []),
+              ),
+            ),
+            Positioned(
+              left: 0,
+              top: 376,
+              child: Container(
+                width: 390,
+                height: 624,
                 decoration: const BoxDecoration(
                   image: DecorationImage(
                     image: NetworkImage("https://via.placeholder.com/390x624"),
@@ -103,8 +116,8 @@ class ChallengeDetailScreen extends StatelessWidget {
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
