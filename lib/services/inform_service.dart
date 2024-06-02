394aae16fd3f588c4f99cf4b4dd953eb8a4d3200
kevinmj12 <kevinmj12@gmail.com>
2024-06-02 14:07:56 +0900

    흡연구역 제보 서버와 연동 완료

--- lib/services/inform_service.dart
+++ lib/services/inform_service.dart
@@ -1,18 +1,46 @@
+import 'dart:convert';
+
+import 'package:damyo/models/sa_inform_model.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
-Future<void> informSmokingArea(Map<String, dynamic> body) async {
+Future<bool> informSmokingArea(SaInformModel saInformModel) async {
   final baseUrl = dotenv.get('BASE_URL');
   var url = Uri.parse('$baseUrl/area/postArea');
+
+  var data = {
+    "address": saInformModel.address,
+    "name": saInformModel.name,
+    "description": saInformModel.description,
+    "latitude": saInformModel.latitude,
+    "longitude": saInformModel.longitude,
+    "score": saInformModel.score,
+    "opened": saInformModel.opened,
+    "closed": saInformModel.closed,
+    "indoor": saInformModel.indoor,
+    "outdoor": saInformModel.outdoor,
+    "url": saInformModel.url,
+  };
+
+  var body = json.encode(data);
+  print(body);
+
   var response = await http.post(
     url,
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: body,
   );
 
+  var resoponseDecode = jsonDecode(utf8.decode(response.bodyBytes));
   if (response.statusCode == 200) {
     print("success informed");
+    print(resoponseDecode);
+    return true;
   } else {
-    print(response.body);
+    print(resoponseDecode);
     throw Exception("failed inform");
   }
 }
