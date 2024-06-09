2dfedbd56aa39d35ac32013260861ee0e88da34e
kevinmj12 <kevinmj12@gmail.com>
2024-06-09 16:59:57 +0900

    로그인 시 유저정보 받아오기

--- lib/services/user_controller_service.dart
+++ lib/services/user_controller_service.dart
@@ -1,21 +1,21 @@
 import 'dart:convert';
 import 'package:damyo/models/userinfo/user_info_model.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
-Future<UserInfoModel?> getUserInfo() async {
+Future<UserInfoModel> getUserInfo() async {
+  FlutterSecureStorage secureStorage = const FlutterSecureStorage();
   final baseUrl = dotenv.get('BASE_URL');
-  final token = dotenv.get('TEST_TOKEN');
+  final accessToken = await secureStorage.read(key: "accessToken");
+  print("token: $accessToken");
 
   var url = Uri.parse('$baseUrl/user/info');
   var headers = {
-    "Authorization": 'Bearer $token',
+    "Authorization": 'Bearer $accessToken',
   };
 
-  var response = await http.get(
-    url,
-    headers: headers
-  );
+  var response = await http.get(url, headers: headers);
 
   var responseDecode = jsonDecode(utf8.decode(response.bodyBytes));
 
@@ -27,5 +27,3 @@ Future<UserInfoModel?> getUserInfo() async {
     throw Exception("Failed to update name");
   }
 }
-
- 
